7d590e8feed3b5bd1b00a0d357a1c3849a459426
Samuel Christian <samuel.christian@kiranatama.com>
2015-08-27 15:37:23 +0700

    delete old bootstrap js and css, change to bootstrap gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
+//= require twitter/bootstrap
 //= require jquery_ujs
-//= require bootstrap
 //= require turbolinks
 //= require application
 //= require_tree .
