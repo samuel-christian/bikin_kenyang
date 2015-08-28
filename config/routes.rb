c682948b5a2ff74ad9f09be795220c89702917b1
Samuel Christian <samuel.christian@kiranatama.com>
2015-08-28 10:54:09 +0700

    devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -6,7 +7,6 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   root 'home#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
