3cb1207259e988e4f3eb40fcce2c9378b113657f
Samuel Christian <samuel.christian@kiranatama.com>
2015-09-21 23:01:19 +0700

    controller filter

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,7 +1,23 @@
 class Admin::AdminController < ApplicationController
+	before_action :authenticate_admin
 	layout 'admin'
 
 	def index
 		
+	end
+
+	def is_admin(user)
+		!user.admin.zero? ? true : false
+	end
+
+	private
+	def authenticate_admin
+		unless current_user
+			if is_admin(current_user)
+				redirect_to admin_root_path
+			else
+				render :file => 'public/403.html', :status => :forbidden, :layout => false
+			end
+		end
 	end
 end
